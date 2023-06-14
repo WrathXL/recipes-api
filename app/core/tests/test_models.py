1a28a1577ff0387da1ec8bd346f1dc4a27ffdf34
WrathXL <christianrd96@gmail.com>
2023-06-14 17:40:48 -0400

    added modifications to the user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,23 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        test_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.Com", "Test2@example.com"],
+        ]
+
+        for email, expected in test_emails:
+            user = get_user_model().objects.create_user(email=email, password="1234")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email=None, password="1234")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser("test@exampl.com", "1234")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
