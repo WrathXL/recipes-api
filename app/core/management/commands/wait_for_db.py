1ed11ea08b207928c7ff1257ebe2a07a2e47eb8c
WrathXL <christianrd96@gmail.com>
2023-06-14 14:57:14 -0400

    fixing linter errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,16 +7,16 @@ from django.core.management.base import BaseCommand
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_connected = False
         while not db_connected:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_connected = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available!'))
-
+        self.stdout.write(self.style.SUCCESS("Database available!"))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,26 +1,26 @@
 """Test Django management commands"""
 
 from unittest.mock import patch
-from psycopg2 import  OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch("core.management.commands.wait_for_db.Command.check")
 class CommandTest(SimpleTestCase):
     def test_db_ready(self, patched_check):
         """Test waiting for db when db is available"""
         patched_check.return_value = True
 
-        call_command('wait_for_db')
-        patched_check.assert_called_once_with(databases=['default'])
+        call_command("wait_for_db")
+        patched_check.assert_called_once_with(databases=["default"])
 
-    @patch('time.sleep')
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 3 + [OperationalError] * 2  + [True]
-
-        call_command('wait_for_db')
+        patched_check.side_effect = (
+            [Psycopg2Error] * 3 + [OperationalError] * 2 + [True]
+        )
+        call_command("wait_for_db")
         self.assertEqual(patched_check.call_count, 6)
-
